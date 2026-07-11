7b1fb4a8ce9add76650779f0a6c75ed5fcefc6e0
Zeeshan <zeeshan9or@gmail.com>
2026-07-11 11:37:42 +0600

    Upload tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+          900: '#0f172a',
+          800: '#1e293b',
+          700: '#334155',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
